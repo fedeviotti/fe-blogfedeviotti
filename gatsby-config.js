ddfba93a42bade465b8f0d45c3413c753ebb6ddc
fedeviotti <fedeviotti@gmail.com>
2021-10-18 23:03:53 +0200

    siteMetadata configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Federico Viotti's blog`,
+    description: `The wonderful blog of Federico Viotti, powered by Gatsby and Sanity.`,
+    author: `@fedeviotti`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
